381970d7e0f6b3fca919ec69226cda05799e8fa5
denglq <denglq@qssec.com>
2016-08-09 14:14:56 +0800

    Revert "exp merge bajie"
    This reverts commit 3a97c822055d56fc368dabb5a30dc0e6494440c5.

--- first.c
+++ first.c
@@ -6,7 +6,6 @@ int main(void)
 {
 printf("experimental before merege");
 printf("this is bajie\n");
-printf("this is experimental\n");
 	printf("hello world!\n");
 	printf("experimental tag\n");
 	printf("experimental tag 2\n");
